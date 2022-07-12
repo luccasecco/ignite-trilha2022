bba793400ffc0d401c4ae52a4f6cd8be1f1961a1
luccasecco <luccasecco@hotmail.com>
2022-07-12 15:02:35 -0300

    adiciona assets e theme default

---
+++ desafio-02/src/styles/themes/default.ts
@@ -0,0 +1,20 @@
+export const defaultTheme = {
+  'gray-100': '#F3F2F2',
+  'gray-200': '#EDEDED',
+  'gray-300': '#E6E5E5',
+  'gray-400': '#8D8686',
+  'gray-500': '#574F4D',
+  'gray-600': '#403937',
+  'gray-900': '#272221',
+
+  'purple-100': '#EBE5F9',
+  'purple-500': '#8047F8',
+  'purple-900': '#4B2995',
+
+  'yellow-100': '#F1E9C9',
+  'yellow-500': '#DBAC2C',
+  'yellow-900': '#C47F17',
+
+  white: '#FFFFFF',
+  'white-100': '#FAFAFA',
+}
