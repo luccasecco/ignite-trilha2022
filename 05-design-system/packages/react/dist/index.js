92df765b322ae327fe766bc0e67939cc1714378d
luccasecco <luccasecco@hotmail.com>
2022-10-18 09:03:56 -0300

    TextArea component

--- 05-design-system/packages/react/dist/index.js
+++ 05-design-system/packages/react/dist/index.js
@@ -31,6 +31,7 @@ __export(src_exports, {
   Button: () => Button,
   Heading: () => Heading,
   Text: () => Text,
+  TextArea: () => TextArea,
   TextInput: () => TextInput
 });
 module.exports = __toCommonJS(src_exports);
@@ -374,6 +375,32 @@ function TextInput({ prefix, ...props }) {
     ]
   });
 }
+
+// src/components/TextArea.tsx
+var TextArea = styled("textarea", {
+  backgroundColor: "&gray900",
+  padding: "$3 $4",
+  borderRadius: "$sm",
+  boxSizing: "border-box",
+  border: "2px solid $gray900",
+  fontFamily: "$default",
+  fontSize: "$sm",
+  color: "$white",
+  fontWeight: "$regular",
+  resize: "vertical",
+  minHeight: 80,
+  "&:focus": {
+    outline: 0,
+    borderColor: "$ignite300"
+  },
+  "&:disabled": {
+    opacity: 0.5,
+    cursor: "not-allowed"
+  },
+  "&:placeholder": {
+    color: "$gray4600"
+  }
+});
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
   Avatar,
@@ -381,5 +408,6 @@ function TextInput({ prefix, ...props }) {
   Button,
   Heading,
   Text,
+  TextArea,
   TextInput
 });
