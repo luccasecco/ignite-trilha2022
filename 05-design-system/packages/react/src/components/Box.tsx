ef3ce5ed82a87cf452f6f1be334d7a9ae1adc5ef
luccasecco <luccasecco@hotmail.com>
2022-10-25 19:57:09 -0300

    change button border 2px

--- 05-design-system/packages/react/src/components/Box.tsx
+++ 05-design-system/packages/react/src/components/Box.tsx
@@ -5,7 +5,7 @@ export const Box = styled('div', {
   padding: '$4',
   borderRadius: '$md',
   backgroundColor: '$gray800',
-  border: '1px solid $gray600',
+  border: '2px solid $gray600',
 })
 
 export interface BoxProps extends ComponentProps<typeof Box> {
